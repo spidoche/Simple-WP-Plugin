439413bddb3abb917ab453cc53a07066b175f6bc
spidoche <spidoche@Nicolass-MacBook-Pro.local>
2015-08-10 12:43:35 +0900

    remove tabs

--- admin/options.php
+++ admin/options.php
@@ -3,10 +3,10 @@
 // ADD ADMIN PAGE
 /*****************************************************************/
 function prefix_add_menu_page() {
-	
-	add_menu_page( 'My plugin', 'My plugin menu', 'manage_options', 'prefix_', 'prefix_options_template', '' );
-	//add_menu_page( $page_title, $menu_title, $capability, $menu_slug, $function, $icon_url, $position );
-	
+
+    add_menu_page( 'My plugin', 'My plugin menu', 'manage_options', 'prefix_', 'prefix_options_template', '' );
+    //add_menu_page( $page_title, $menu_title, $capability, $menu_slug, $function, $icon_url, $position );
+
 }
 add_action( 'admin_menu', 'prefix_add_menu_page' );
 
@@ -15,18 +15,18 @@ add_action( 'admin_menu', 'prefix_add_menu_page' );
 // INIT THE OPTIONS PAGE TEMPLATE : used in prefix_add_menu_page()
 /*****************************************************************/
 function prefix_options_template() {
-	
-    include( PREFIX_PATH . '/admin/options_page.php');	
+
+    include( PREFIX_PATH . '/admin/options_page.php');
 }
 
 
 /*****************************************************************/
 // REGISTERS PLUGIN OPTIONS SETTINGS
-/*****************************************************************/ 
-function prefix_register_settings() { 
+/*****************************************************************/
+function prefix_register_settings() {
 
     register_setting( 'prefix_options', 'prefix_' );
-    
+
 }
 add_action('admin_init', 'prefix_register_settings');
 
